4ba2dc10cfd6c32c02222efc8ec05ec92e2b0b42
jm <jhn.meelr@gmail.com>
2017-11-22 21:51:57 +0200

    Create the Basic React Native Todo Application Layout

---
+++ footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
